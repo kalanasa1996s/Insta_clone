30ef8194a2c0c584d8a519e149ff0302fed3db97
kalanasa1996s <kalanasa1996@gmail.com>
2021-02-18 17:47:33 +0530

    bottom bar

--- lib/insta_home.dart
+++ lib/insta_home.dart
@@ -13,6 +13,48 @@ class MyHomePage extends StatelessWidget {
           height: 35.0,
           child: Image.asset("assets/images/insta_logo.png"),
         ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.send,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        height: 50.0,
+        alignment: Alignment.center,
+        child: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.add_box),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.account_box),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
